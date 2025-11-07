8423ce1f78a91433de0335567d1ff33a823f4027
専門学校 東京ネットウエイブ <visualstudio.tnw.game@outlook.com>
2025-11-07 12:23:44 +0900

    v1

--- Assets/Wizard - 2D Character/PlayerController.cs
+++ Assets/Wizard - 2D Character/PlayerController.cs
@@ -2,22 +2,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rb2d;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public float dashDistance = 3f; // ÑÅÑÇÑÄÑÉÑÑÑpÑë ÑtÑyÑÉÑÑÑpÑ~ÑàÑyÑë ÑÇÑçÑrÑ{Ñp
+
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        // ÑÅÑÇÑÄÑÉÑÑÑÄÑz ÑÇÑçÑrÑÄÑ{: ÑÖÑtÑuÑÇÑwÑyÑrÑpÑÑÑé Shift Ñy Ñ~ÑpÑwÑpÑÑÑé ÑÉÑÑÑÇÑuÑ|Ñ{ÑÖ -> Ñ}ÑsÑ~ÑÄÑrÑuÑ~Ñ~ÑÄ ÑÉÑ}ÑuÑÉÑÑÑyÑÑÑéÑÉÑë Ñ~Ñp dashDistance
+        bool shift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        if (shift && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * dashDistance;
+        }
+        else if (shift && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * dashDistance;
+        }
+        else if (shift && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * dashDistance;
+        }
+        else if (shift && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * dashDistance;
+        }
 
-        rb2d.linearVelocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+        // ÑÄÑqÑçÑâÑ~ÑÄÑu ÑtÑrÑyÑwÑuÑ~ÑyÑu
+        float moveAmount = speed * Time.deltaTime;
 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * moveAmount, Space.World);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * moveAmount, Space.World);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * moveAmount, Space.World);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * moveAmount, Space.World);
+        }
     }
 }
