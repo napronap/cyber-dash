1ef6bd38e624c9dc4226203fe1925ad16b46e40d
İlker Bozkurt <43248549+nowaljin@users.noreply.github.com>
2025-12-19 11:17:57 +0900

    player component (#5)
    * v1
    
    * v1
    
    * v2
    
    * 12.11ver
    
    * Update SampleScene.unity
    
    * 14.11
    
    * 19.11
    
    * 21.11
    
    * v1201
    
    * v1203
    
    * 1205
    
    * v1208
    
    * check can dash
    
    * ground check and dash settings
    
    v1
    
    * map size checkup
    
    start
    
    * weapon delete
    
    s
    
    * delete commented code
    
    * delete enemy + weapon animation
    
    * neniojjuun
    
    d
    
    ---------
    
    Co-authored-by: Nikita Doroshenko <bakenton@gmail.com>
    Co-authored-by: 専門学校 東京ネットウエイブ <visualstudio.tnw.game@outlook.com>
    Co-authored-by: fran <francoteg+1@gmail.com>

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,149 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting.InputSystem;
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance { get; private set; }
+
+    [SerializeField] private float movingSpeed = 5f;
+    [Header("Dash Settings")]
+    [SerializeField] private int dashSpeed = 4;
+    [SerializeField] private float dashTime = 0.2f;
+    [SerializeField] private  TrailRenderer trailRenderer;
+    [SerializeField] private float dashCooldownTime = 30.0f;
+    [SerializeField] private float jumpForce = 1000f;
+    [SerializeField] private float jumpTime = 0.2f;
+
+    private Rigidbody2D rb;
+    private float minMovingSpeed = 0.1f;
+    private bool isRunning = false;
+    private float _initialMovingSpeed;
+    private bool isDashing;
+    private bool isJumping;
+    private bool isGrounded;
+    private float groundLevel = -20f;
+    private bool canDash;
+    private bool dashStarted = false;
+
+    private void Awake()
+    {
+        Instance = this;
+        rb = GetComponent<Rigidbody2D>();
+        _initialMovingSpeed = movingSpeed;
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPlayerDash += GameInput_OnPlayerDash;
+
+        GameInput.Instance.OnPlayerJump += GameInput_OnPlayerJump;
+
+        isGrounded = transform.position.y < groundLevel;
+    }
+
+    private void GameInput_OnPlayerDash(object sender, System.EventArgs e)
+    {
+        Dash();
+    }
+
+    private void GameInput_OnPlayerJump(object sender, System.EventArgs e)
+    {
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (!isJumping)
+                StartCoroutine(JumpRoutine());
+    }
+    private IEnumerator JumpRoutine()
+    {
+        if (isGrounded)
+        {
+            isJumping = true;
+            rb.AddForce(Vector2.up * jumpForce*10);
+            Debug.Log("Jumped");
+            yield return new WaitForSeconds(jumpTime);
+            isJumping = false;
+        }
+    }
+
+    private void Dash()
+    {
+        if(canDash && !dashStarted)
+            StartCoroutine(DashRoutine());
+
+    }
+
+    private IEnumerator DashRoutine()
+    {
+        isDashing = true;
+        dashStarted = true;
+        movingSpeed *= dashSpeed;
+        trailRenderer.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+
+        trailRenderer.emitting = false;
+        movingSpeed = _initialMovingSpeed;
+
+        yield return new WaitForSeconds(dashCooldownTime);
+        isDashing = false;
+    }
+
+    void FixedUpdate()
+    {
+        HandleMovement();
+
+        isGrounded = CheckIsGrounded();
+
+        if (dashStarted && !isDashing)
+        {
+            if (isGrounded)
+            {
+                canDash = true;
+                dashStarted = false;
+            }
+        } else
+        {
+            canDash = !dashStarted;
+        }
+    }
+
+    private bool CheckIsGrounded()
+    { 
+        return transform.position.y < groundLevel;
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        inputVector = inputVector.normalized;
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+
+        if ((Mathf.Abs(inputVector.x) > minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed))
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+
+        }
+
+
+    }
+    public bool IsRunning()
+    {
+        return isRunning;
+
+    }
+
+    public Vector3 GetPlayerScreenPosition()
+    {
+        Vector3 playerScreenPosition= Camera.main.WorldToScreenPoint(transform.position);
+        return playerScreenPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerVisual.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerVisual : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private const string IS_RUNNING = "IsRunning";
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_RUNNING, PlayerController.Instance.IsRunning());
+        AdjustPlayerFacingDirection();
+
+    }
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos=GameInput.Instance.GetMousePosition();
+        Vector3 playerPosition=PlayerController.Instance.GetPlayerScreenPosition();
+        if (mousePos.x < playerPosition.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+    }
+
+}
