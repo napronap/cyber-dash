a29dc98ff6da712047b93b42d3780480a6b748ba
zelly-fish <215442078+ramzelsuarez@users.noreply.github.com>
2026-01-14 15:19:54 +0900

    PauseUI_updates

--- Assets/Scripts/ReturnToMainMenu.cs
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReturnToMainMenu : MonoBehaviour
 {
     public void GoToMainMenu()
     {
        SceneFadeManager.I.FadeToScene("MainMenuTest");
+    }
+
+    public void NoBackToPause()
+    {
+        SceneManager.LoadScene("Pause");
     }
 }
